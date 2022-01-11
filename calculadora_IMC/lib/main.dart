03060bab1a31ca71beb4aae8d0396c79f8057fcb
ego-prog <ego@ego.adv.br>
2022-01-11 13:18:08 -0300

    Controller e Cálculos

--- calculadora_IMC/lib/main.dart
+++ calculadora_IMC/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
-void main(){
+import 'package:flutter/painting.dart';
+
+void main() {
   runApp(MaterialApp(
     home: Home(),
   ));
 }
+
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -12,20 +15,92 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  TextEditingController weightController = TextEditingController();
+  TextEditingController heightController = TextEditingController();
+  String _infoText = "Informe seus dados";
+
+  void _resetFields() {
+    weightController.text = "";
+    heightController.text = "";
+    setState(() {
+      _infoText = "Informe seus dados";
+    });
+  }
+
+  void _calculate() {
+    setState(() {
+      double weight = double.parse(weightController.text);
+      double height = double.parse(heightController.text) / 100;
+      double imc = weight / (height * height);
+      if (imc < 18.6)
+        _infoText = "Abaixo do Peso (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 18.6 && imc < 24.9)
+        _infoText = "Peso Ideal (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 24.9 && imc < 29.9)
+        _infoText = "Levemente Acima do Peso (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 29.9 && imc < 34.9)
+        _infoText = "Obesidade Grau I (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 34.9 && imc < 39.9)
+        _infoText = "Obesidade Grau II (${imc.toStringAsPrecision(3)})";
+      else if (imc >= 40.0)
+        _infoText = "Obesidade Grau III (${imc.toStringAsPrecision(3)})";
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Calculadora de IMC"),
-        centerTitle: true,
-        backgroundColor: Colors.green,
-        actions: [
-          IconButton(
-              onPressed: (){},
-              icon: Icon(Icons.refresh)
-          )
-        ],
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Calculadora de IMC"),
+          centerTitle: true,
+          backgroundColor: Colors.green,
+          actions: [
+            IconButton(onPressed: _resetFields, icon: Icon(Icons.refresh))
+          ],
+        ),
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          padding: EdgeInsets.fromLTRB(10.0, 0, 10.0, 0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Icon(Icons.person_outline, size: 120, color: Colors.green),
+              TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Peso (kg)",
+                    labelStyle: TextStyle(color: Colors.green)),
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25),
+                controller: weightController,
+              ),
+              TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(
+                      labelText: "Altura (cm)",
+                      labelStyle: TextStyle(color: Colors.green)),
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: Colors.green, fontSize: 25),
+                  controller: heightController),
+              Padding(
+                padding: EdgeInsets.only(top: 10, bottom: 10),
+                child: Container(
+                  height: 50.0,
+                  child: RaisedButton(
+                    onPressed: _calculate,
+                    child: Text("Calcular",
+                        style: TextStyle(color: Colors.white, fontSize: 25.0)),
+                    color: Colors.green,
+                  ),
+                ),
+              ),
+              Text(
+                _infoText,
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.green, fontSize: 25.0),
+              )
+            ],
+          ),
+        ));
   }
 }
